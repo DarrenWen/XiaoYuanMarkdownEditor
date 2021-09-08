e0f605a68efa49d0f0c0ebb4df0e0e3b4623c1dd
fuwenjun <fwj380891124@163.com>
2021-09-08 13:23:12 +0800

    新增托盘图标和系统消息提醒

--- src/XiaoYuanMarkdownEditor/js/index.js
+++ src/XiaoYuanMarkdownEditor/js/index.js
@@ -2,117 +2,140 @@
 const electron = require("electron");
 const ipcRenderer = electron.ipcRenderer;
 const remote = electron.remote;
-var showdown  = require('showdown');
-function load(){
-   // 第一次调用该函数
+var showdown = require('showdown');
+function load() {
+  // 第一次调用该函数
   watchWindowSize();
   SelectedDefaultMenu();
   mdSwitch();
+  Notice();
 }
 
- function SelectedDefaultMenu () {
+function SelectedDefaultMenu() {
+  // 清空控制台
   console.clear();
+  // 设置首次进入页面菜单第一项为选中项
   document.getElementsByClassName('sidebar__menu--item')[0].classList.add('is-active');
+  // 获取菜单项
   const menuItems = Array.from(document.querySelectorAll('.sidebar__menu--item'));
-menuItems.forEach(item => {
-  item.addEventListener('click', e => {
-    menuItems.forEach(item => {
-      item.classList.remove('is-active');
+  // 循环给菜单项添加事件
+  menuItems.forEach(item => {
+    // 添加点击事件
+    item.addEventListener('click', e => {
+      // 先移除菜单项上的所有的选中样式
+      menuItems.forEach(item => {
+        item.classList.remove('is-active');
+      });
+      // 获取前端刚才设置的data扩展属性值
+      var data = e.currentTarget.getAttribute('data');
+      // 设置当前点击菜单样式为选中
+      e.currentTarget.classList.add('is-active');
+      // 执行文件操作
+      fileControl(data);
     });
-    var data = e.currentTarget.getAttribute('data');
-    e.currentTarget.classList.add('is-active');
-    fileControl(data);
   });
-});
 };
 
 
 
 function mdSwitch() {
-    var mdValue = document.getElementById("md-area").value;
-    var converter = new showdown.Converter();
-    var html = converter.makeHtml(mdValue);
-    document.getElementById("show-area").innerHTML = html;
+  var mdValue = document.getElementById("md-area").value;
+  var converter = new showdown.Converter();
+  var html = converter.makeHtml(mdValue);
+  document.getElementById("show-area").innerHTML = html;
 }
 
 // add 从主进程打开github成功后的监听事件
-ipcRenderer.on("readFile",(event,arg)=>{
-    document.title = arg.title;
-    console.log(arg);
-    document.getElementById("md-area").value = arg.content;
-    mdSwitch();
+ipcRenderer.on("readFile", (event, arg) => {
+  document.title = arg.title;
+  console.log(arg);
+  document.getElementById("md-area").value = arg.content;
+  mdSwitch();
 })
 
-ipcRenderer.on('writeFile',(event,arg)=>{
-    var data = document.getElementById("md-area").value;
-    ipcRenderer.send('saveFile',data);
+ipcRenderer.on('writeFile', (event, arg) => {
+  var data = document.getElementById("md-area").value;
+  ipcRenderer.send('saveFile', data);
 });
 
-ipcRenderer.on('createNew',(event,arg)=>{
-    document.title='新建文件';
-    document.getElementById("md-area").value ='';
-    mdSwitch();
+ipcRenderer.on('createNew', (event, arg) => {
+  document.title = '新建文件';
+  document.getElementById("md-area").value = '';
+  mdSwitch();
 })
 
 // renderer
 window.addEventListener('contextmenu', (e) => {
-    //e.preventDefault()
-    //ipcRenderer.send('show-context-menu')
-  });
+  //e.preventDefault()
+  //ipcRenderer.send('show-context-menu')
+});
 
-  // 定义事件侦听器函数
+// 定义事件侦听器函数
 function watchWindowSize() {
-    // 获取窗口的宽度和高度，不包括滚动条
-    var w = document.documentElement.clientWidth;
-    var h = document.documentElement.clientHeight;
-    // 打印结果
-    console.log( "宽: " + w + ", " + "高: " + h);
-    var tbArea =  document.getElementById("tbArea");
-    tbArea.style.height = (h-65)+"px";
-    var showarea = document.getElementById("show-area");
-    showarea.style.height = (h-65)+"px";
-    document.getElementById("md-area").focus();
-  }
-  // 将事件侦听器函数附加到窗口的resize事件
-  window.addEventListener("resize", watchWindowSize);
-  
-  
+  // 获取窗口的宽度和高度，不包括滚动条
+  var w = document.documentElement.clientWidth;
+  var h = document.documentElement.clientHeight;
+  // 打印结果
+  console.log("宽: " + w + ", " + "高: " + h);
+  var tbArea = document.getElementById("tbArea");
+  tbArea.style.height = (h - 65) + "px";
+  var showarea = document.getElementById("show-area");
+  showarea.style.height = (h - 65) + "px";
+  document.getElementById("md-area").focus();
+}
+// 将事件侦听器函数附加到窗口的resize事件
+window.addEventListener("resize", watchWindowSize);
+
+
 ipcRenderer.on('context-menu-command1', (e, command) => {
-// ...
-   alert(command);
+  // ...
+  alert(command);
 })
 
 ///控制窗体变化
 function winControl(action) {
   const browserWindow = remote.getCurrentWindow()
   switch (action) {
-      case 'minimize':
-          browserWindow.minimize()
-          break;
-      case 'maximize':
-          // if (this.isMaximized) {
-          if (browserWindow.isMaximized()) {
-              browserWindow.unmaximize()
-          } else {
-              if (this.isMaximized) {
-                  browserWindow.unmaximize()
-              } else {
-                  browserWindow.maximize()
-              }
-          }
-          // this.isMaximized = browserWindow.isMaximized()
-          this.isMaximized = !this.isMaximized
+    case 'minimize':
+      browserWindow.minimize()
+      break;
+    case 'maximize':
+      // if (this.isMaximized) {
+      if (browserWindow.isMaximized()) {
+        browserWindow.unmaximize()
+      } else {
+        if (this.isMaximized) {
+          browserWindow.unmaximize()
+        } else {
+          browserWindow.maximize()
+        }
+      }
+      // this.isMaximized = browserWindow.isMaximized()
+      this.isMaximized = !this.isMaximized
 
-          break;
-      case 'close':
-          browserWindow.close()
-          break;
-      default:
-          break;
+      break;
+    case 'close':
+      browserWindow.close()
+      break;
+    default:
+      break;
   }
 }
 
-function fileControl(action){
-  ipcRenderer.send('filecontrol',action);
- 
+function fileControl(action) {
+  ipcRenderer.send('filecontrol', action);
+
+}
+
+function Notice(){
+  var n = new Notification('状态更新提醒',{
+    body: '你的朋友圈有3条新状态，快去查看吧',
+    tag: 'linxin',
+    icon: 'https://www.baidu.com/img/PCfb_5bf082d29588c07f842ccde3f97243ea.png',
+    requireInteraction: true
+})
+
+setTimeout(() => {
+  n.close()
+}, 10000);
 }
--- src/XiaoYuanMarkdownEditor/main.js
+++ src/XiaoYuanMarkdownEditor/main.js
@@ -2,167 +2,206 @@
 
 // add 新增ipcMain,shell引用
 const { info } = require("console");
-const { app, BrowserWindow, ipcMain,shell,Menu,dialog } = require("electron");
-const file  = require('./js/file');
+const { app, BrowserWindow, ipcMain, shell, Menu, dialog, Tray,Notification  } = require("electron");
+const file = require('./js/file');
 
 const path = require("path");
 const { title } = require("process");
 var mainWindow = null;
+let appIcon = null
+const NOTIFICATION_TITLE = '小猿MarkDown编辑器'
+const NOTIFICATION_BODY = '欢迎使用小猿MarkDown编辑器'
+const CLICK_MESSAGE = 'Notification clicked'
+function showNotification () {
+ var notify = new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY });
+ 
+ notify.onclick = () => console.log(CLICK_MESSAGE)
+ notify.show();
+}
+
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule:true
-        },
-        frame: false
-    });
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
-    
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    },
+    frame: false
+  });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
 
 
-    // mainWindow.loadURL('https://github.com');
+  // mainWindow.loadURL('https://github.com');
+  appIcon = new Tray(path.join(__dirname, 'static/ico/logo.ico'))
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '最小化', click: () => {
+        mainWindow.minimize();
+      }
+    },
+    {
+      label: '关闭', click: async () => {
+        app.quit();
+      }
+    }
+  ])
+
+
+  // Make a change to the context menu
+  contextMenu.items[1].checked = false
+  appIcon.setToolTip('小猿MarkDown编辑器.')
+  // Call this again for Linux because we modified the context menu
+  appIcon.setContextMenu(contextMenu)
+
+  showNotification();
 }
 //监听 electron ready 事件创建窗口
 app.on('ready', createWindow);
 //监听窗口关闭的事件，关闭的时候退出应用，macOs 需要排除
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 //Macos 中点击 dock 中的应用图标的时候重新创建窗口
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 // add 演示主进程和渲染通信代码
 ipcMain.on('opengithub', (event, arg) => {
-    console.log(arg) // prints "ping"
-    event.reply('githubopened', '打开github成功!')
-    shell.openExternal(arg);
+  console.log(arg) // prints "ping"
+  event.reply('githubopened', '打开github成功!')
+  shell.openExternal(arg);
 });
 
 // 添加菜单相关逻辑
 const isMac = process.platform === 'darwin' //判断操作系统版本是不是mac
 let filePath = '';//需要保存的文件路径
 const template = [
-    // { role: 'appMenu' }
-    ...(isMac ? [{
-      label: app.name,
-      submenu: [
-        { role: 'about' },
-        { type: 'separator' },
-        { role: 'services' },
-        { type: 'separator' },
-        { role: 'hide' },
-        { role: 'hideothers' },
-        { role: 'unhide' },
-        { type: 'separator' },
-        { role: 'quit' }
-      ]
-    }] : []),
-    // { role: 'fileMenu' }
-    {
-      label: '文件',
-      submenu: [
-        {label:'新建',click:async()=>{
-            CreateFile();
-        }},
-        {label:'打开',click:async()=>{
-            OpenFile();
-        }},
-        {label:'保存',click:async()=>{
-            SaveFile();
-        }},
-        isMac ? { role: 'close' } : { role: 'quit',label:"退出" }
-      ]
-    },
-    // { role: 'editMenu' }
-    {
-      label: '编辑',
-      submenu: [
-        { role: 'undo',label:"撤销" },
-        { role: 'redo',label:'恢复' },
-        { type: 'separator' },
-        { role: 'cut',label:'剪切' },
-        { role: 'copy',label:'复制' },
-        { role: 'paste',label:'粘贴' },
-        ...(isMac ? [
-          { role: 'pasteAndMatchStyle' },
-          { role: 'delete' },
-          { role: 'selectAll' },
-          { type: 'separator' },
-          {
-            label: 'Speech',
-            submenu: [
-              { role: 'startSpeaking' },
-              { role: 'stopSpeaking' }
-            ]
-          }
-        ] : [
-          { role: 'delete',label:'删除' },
-          { type: 'separator' },
-          { role: 'selectAll',label:'全选' }
-        ])
-      ]
-    },
-    // { role: 'viewMenu' }
-    {
-      label: '视图',
-      submenu: [
-        { role: 'reload',label:'重新加载' },
-        { role: 'forceReload',label:'强制重新加载' },
-        { role: 'toggleDevTools',label:'打开调试工具' },
-        { type: 'separator' },
-        { role: 'resetZoom',label:'重置缩放' },
-        { role: 'zoomIn',label:'放大' },
-        { role: 'zoomOut',label:'缩小' },
-        { type: 'separator' },
-        { role: 'togglefullscreen',label:'全屏' }
-      ]
-    },
-    // { role: 'windowMenu' }
-    {
-      label: '窗口',
-      submenu: [
-        { role: 'minimize',label:'最小化' },
-        { role: 'zoom' },
-        ...(isMac ? [
-          { type: 'separator' },
-          { role: 'front' },
-          { type: 'separator' },
-          { role: 'window' }
-        ] : [
-          { role: 'close',label:'关闭' }
-        ])
-      ]
-    },
-    {
-      role: 'help',
-      label:'帮助',
-      submenu: [
-        {
-          label: '学习更多',
-          click: async () => {
-            const { shell } = require('electron')
-            await shell.openExternal('https://electronjs.org')
-          }
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: '文件',
+    submenu: [
+      {
+        label: '新建', click: async () => {
+          CreateFile();
         }
-      ]
-    }
-  ];
-  
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+      },
+      {
+        label: '打开', click: async () => {
+          OpenFile();
+        }
+      },
+      {
+        label: '保存', click: async () => {
+          SaveFile();
+        }
+      },
+      isMac ? { role: 'close' } : { role: 'quit', label: "退出" }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: '编辑',
+    submenu: [
+      { role: 'undo', label: "撤销" },
+      { role: 'redo', label: '恢复' },
+      { type: 'separator' },
+      { role: 'cut', label: '剪切' },
+      { role: 'copy', label: '复制' },
+      { role: 'paste', label: '粘贴' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete', label: '删除' },
+        { type: 'separator' },
+        { role: 'selectAll', label: '全选' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: '视图',
+    submenu: [
+      { role: 'reload', label: '重新加载' },
+      { role: 'forceReload', label: '强制重新加载' },
+      { role: 'toggleDevTools', label: '打开调试工具' },
+      { type: 'separator' },
+      { role: 'resetZoom', label: '重置缩放' },
+      { role: 'zoomIn', label: '放大' },
+      { role: 'zoomOut', label: '缩小' },
+      { type: 'separator' },
+      { role: 'togglefullscreen', label: '全屏' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: '窗口',
+    submenu: [
+      { role: 'minimize', label: '最小化' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close', label: '关闭' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    label: '帮助',
+    submenu: [
+      {
+        label: '学习更多',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+];
 
-  // main
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+// main
 ipcMain.on('show-context-menu', (event) => {
   const template = [
     {
@@ -176,74 +215,74 @@ ipcMain.on('show-context-menu', (event) => {
   menu.popup(BrowserWindow.fromWebContents(event.sender))
 })
 
-ipcMain.on('saveFile',(event,args)=>{
-  file.saveFile(filePath,args);
-  mainWindow.webContents.send('readFile',{title:filePath,content:args});
-  dialog.showMessageBox({title:'系统提示',message:'保存成功！'});
+ipcMain.on('saveFile', (event, args) => {
+  file.saveFile(filePath, args);
+  mainWindow.webContents.send('readFile', { title: filePath, content: args });
+  dialog.showMessageBox({ title: '系统提示', message: '保存成功！' });
 
 });
 
-ipcMain.on('filecontrol',(event,args)=>{
+ipcMain.on('filecontrol', (event, args) => {
   switch (args) {
     case 'create':
       CreateFile();
       break;
-      case 'open':
-        OpenFile();
+    case 'open':
+      OpenFile();
       break;
-      case 'save':
-        SaveFile();
+    case 'save':
+      SaveFile();
       break;
-      case 'tool':
-        mainWindow.webContents.openDevTools();
-        break;
-        case 'help':
-          var fpath = path.join(__dirname,"readme.md");
-          var data = file.openFile(fpath);
-        mainWindow.webContents.send('readFile',{title:fpath,content:data});
-        
-        break;
+    case 'tool':
+      mainWindow.webContents.openDevTools();
+      break;
+    case 'help':
+      var fpath = path.join(__dirname, "readme.md");
+      var data = file.openFile(fpath);
+      mainWindow.webContents.send('readFile', { title: fpath, content: data });
+
+      break;
   }
 });
 
-function CreateFile(){
+function CreateFile() {
   filePath = '';
   mainWindow.webContents.send('createNew');
 }
-function SaveFile(){
-  if(filePath==''){
-    dialog.showMessageBox({title:'提示',message:'确认要保存吗？',type:"question",buttons:['确认','取消'],noLink:true}).then((res)=>{
-      if(res.response==0){
-          dialog.showSaveDialog({title:'保存文件',filters:[{name:'文本文件',extensions:['md']}]}).then((res)=>{
-            console.log(res);
-            if(!res.canceled){
-              filePath = res.filePath;
-              mainWindow.webContents.send('writeFile');
-            }
-              
-          }).catch((err)=>{
-              console.log(err);
-          });
+function SaveFile() {
+  if (filePath == '') {
+    dialog.showMessageBox({ title: '提示', message: '确认要保存吗？', type: "question", buttons: ['确认', '取消'], noLink: true }).then((res) => {
+      if (res.response == 0) {
+        dialog.showSaveDialog({ title: '保存文件', filters: [{ name: '文本文件', extensions: ['md'] }] }).then((res) => {
+          console.log(res);
+          if (!res.canceled) {
+            filePath = res.filePath;
+            mainWindow.webContents.send('writeFile');
+          }
+
+        }).catch((err) => {
+          console.log(err);
+        });
       }
-      else{
-          dialog.showMessageBox({title:'系统提示',message:'用户取消了操作！',type:'warning'})
+      else {
+        dialog.showMessageBox({ title: '系统提示', message: '用户取消了操作！', type: 'warning' })
       }
-  });
-}else{
-  mainWindow.webContents.send('writeFile');
+    });
+  } else {
+    mainWindow.webContents.send('writeFile');
+  }
 }
-}
-function OpenFile(){
-  dialog.showOpenDialog({title:'打开文件',properties:['openfile'],filters:[{name:'文本文件',extensions:['md']}]}).then((res)=>{
-    if(!res.canceled){
-       console.log(res.filePaths.toString());
-       filePath = res.filePaths[0];
+function OpenFile() {
+  dialog.showOpenDialog({ title: '打开文件', properties: ['openfile'], filters: [{ name: '文本文件', extensions: ['md'] }] }).then((res) => {
+    if (!res.canceled) {
+      console.log(res.filePaths.toString());
+      filePath = res.filePaths[0];
       var data = file.openFile(res.filePaths[0]);
-      mainWindow.webContents.send('readFile',{title:res.filePaths[0],content:data});
+      mainWindow.webContents.send('readFile', { title: res.filePaths[0], content: data });
     }
-      // console.log(res.filePaths.toString());
-      // dialog.showMessageBox({title:'打开文件提醒',message:res.filePaths.toString(),type:'info'});
-  }).catch((err)=>{
-      console.log(err);
+    // console.log(res.filePaths.toString());
+    // dialog.showMessageBox({title:'打开文件提醒',message:res.filePaths.toString(),type:'info'});
+  }).catch((err) => {
+    console.log(err);
   });
 }
